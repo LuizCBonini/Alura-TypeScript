885012ddc8b0d3ff0a97ba25b2c9fd5b25e66d59
Luiz Bonini <88125049+LuizCBonini@users.noreply.github.com>
2022-08-22 16:05:47 -0300

    TS 2.1.5

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,5 @@
 import { NegociacaoController } from "./controllers/negociacao.controller.js";
+import { NegociacoesView } from "./views/negociacoes-view.js";
 
 
 const controller = new NegociacaoController();
@@ -8,5 +9,6 @@ form.addEventListener('submit', event => {
     controller.adiciona();
 })
 
-
-
+const negociacoesView = new NegociacoesView;
+const template = negociacoesView.template();
+console.log(template);
